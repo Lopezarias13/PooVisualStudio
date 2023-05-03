cc911fb30aef250fbe56d2cf6cf0cf802ac4f280
José Arias <joseguiller1300@gmail.com>
2023-05-03 10:30:41 -0600

    Poo Vehiculos

--- Ejer1/Ejer1/Program.cs
+++ Ejer1/Ejer1/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*1. ejercicio 1. Crea una clase Vehiculo con las siguientes propiedades:
+– Id, Marca, Modelo, KM, Precio, Año
+
+Debemos crear un constructor donde le pasemos los valores. Crea sus get y set de cada propiedad.
+Sobrescriba el metodo toString(). El cual muestre en pantalla la marca, el modelo el año y el precio del vehículo*/
 
 namespace Ejer1
 {
@@ -10,6 +15,9 @@ namespace Ejer1
     {
         static void Main(string[] args)
         {
+            Vehiculo v1 = new Vehiculo(1, "Ford", "Fiesta", 10000, 10000, 2010);
+            Console.WriteLine(v1.ToString());
+            Console.ReadKey();
         }
     }
 }
---
+++ Ejer1/Ejer1/Vehiculo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer1
+{
+    internal class Vehiculo
+    {
+        int id;
+        string marca;
+        string modelo;
+        int km;
+        int precio;
+        int annio;
+
+        public Vehiculo(int id, string marca, string modelo, int km, int precio, int annio)
+        {
+            this.Id = id;
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.Km = km;
+            this.Precio = precio;
+            this.Annio = annio;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public int Km { get => km; set => km = value; }
+        public int Precio { get => precio; set => precio = value; }
+        public int Annio { get => annio; set => annio = value; }
+
+        public override string ToString()
+        {
+            return "Modelo: " + modelo + "\nMarca: " + marca + "\nAño: " + annio + "\nPrecio: " + precio + "\n";
+        }
+    }
+}
