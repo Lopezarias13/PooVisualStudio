2edb1286f3ec6fca7b5b88ec082d92d03b738b23
José Arias <joseguiller1300@gmail.com>
2023-05-04 14:34:24 -0600

    Fotos

---
+++ Ejer5/Ejer5/Foto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer5
+{
+    internal class Foto
+    {
+        int idFoto;
+        int tamanoMB;
+        DateTime fechaCreacion;
+        string comentarios;
+
+        public Foto(int idFoto, int tamanoMB, DateTime fechaCreacion, string comentarios)
+        {
+            this.IdFoto = idFoto;
+            this.TamanoMB = tamanoMB;
+            this.FechaCreacion = fechaCreacion;
+            this.Comentarios = comentarios;
+        }
+
+        public int IdFoto { get => idFoto; set => idFoto = value; }
+        public int TamanoMB { get => tamanoMB; set => tamanoMB = value; }
+        public DateTime FechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
+        public string Comentarios { get => comentarios; set => comentarios = value; }
+
+        public override string ToString()
+        {
+            return $"Id: {IdFoto}\nTamaño: {TamanoMB} MB\nFecha de creación: {FechaCreacion}\nComentarios: {Comentarios}";
+        }
+    }
+    class Album
+    {
+        int id;
+        string nombre;
+        DateTime fechaCreacion;
+        List<Foto> fotos;
+        public Album(int id, string nombre, DateTime fechaCreacion, List<Foto> fotos)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.FechaCreacion = fechaCreacion;
+            this.Fotos = fotos;
+        }
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public DateTime FechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
+        internal List<Foto> Fotos { get => fotos; set => fotos = value; }
+        public void AniadirFoto(Foto f)
+        {
+            Fotos.Add(f);
+        }
+        public void MostrarFoto()
+        {
+            foreach (Foto f in Fotos)
+            {
+                Console.WriteLine(f);
+            }
+        }
+        public void VaciarAlbum()
+        {
+            Fotos.Clear();
+        }
+        public void EliminarFoto(Foto f)
+        {
+            Fotos.Remove(f);
+        }
+        public double TamanoAlbum()
+        {
+            double tamano = 0;
+            foreach (Foto f in Fotos)
+            {
+                tamano += f.TamanoMB;
+            }
+            return tamano / 1024;
+        }
+    }   
+}
---
+++ Ejer5/Ejer5/Program.cs
@@ -0,0 +1,53 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*5.  Cree un clase Foto la cual tendrá los siguientes campos:
+-Id foto
+-Tamaño en MB
+-Fecha Creación
+-Comentarios
+
+Cree su respectivo constructor y propiedades
+
+Sobrescriba el método toString(). El cual muestre en pantalla la información almacenada de la foto
+A continuación cree una clase Álbum la cual deberá tener los siguientes campos
+-Id
+-Nombre
+-Fecha Creación
+-Lista de Fotos
+
+Cree su respectivo constructor y propiedades
+Crearemos los siguientes métodos:
+– aniadirFoto(Foto f): Permite añadir una nueva foto 
+– mostrarFoto(): Muestra la lista de las foto registradas
+– vaciarAlbum(): Elimina todos las fotos registradas
+– eliminarFoto(Foto f).
+-TamanoAlbum(): Muestra el tamaño en GB del álbum.*/
+
+namespace Ejer5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Foto foto = new Foto(1, 2, DateTime.Now, "Comentario");
+            Album album = new Album(1, "Album1", DateTime.Now, new List<Foto>());
+            Console.WriteLine(album.ToString());
+            album.AniadirFoto(foto);
+            Console.WriteLine(album.ToString());
+            album.MostrarFoto();
+            album.VaciarAlbum();
+            Console.WriteLine(album.ToString());
+
+            
+
+            
+
+
+
+        }
+    }
+}
