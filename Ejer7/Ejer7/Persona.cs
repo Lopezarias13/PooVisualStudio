77514cc7fba7bd00361616097dff614518aa8658
José Arias <joseguiller1300@gmail.com>
2023-05-04 20:54:45 -0600

    2cambio

--- Ejer7/Ejer7/Persona.cs
+++ Ejer7/Ejer7/Persona.cs
@@ -8,5 +8,35 @@ namespace Ejer7
 {
     internal class Persona
     {
+        string nombre;
+        public Persona(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public override string ToString()
+        {
+            return "Nombre: " + nombre + "\n";
+        }
+    }
+    class Estudiante : Persona
+    {
+        public Estudiante(string nombre) : base(nombre)
+        {
+        }
+        public void Estudiar()
+        {
+            Console.WriteLine("Estudiar");
+        }
+    }
+    class Profesor : Persona
+    {
+        public Profesor(string nombre) : base(nombre)
+        {
+        }
+        public void Explicar()
+        {
+            Console.WriteLine("Explicar");
+        }
     }
 }
--- Ejer7/Ejer7/Program.cs
+++ Ejer7/Ejer7/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*7. Crea un programa en C# que solicite tres nombres de personas al usuario y los almacene en un array de objetos de tipo Persona. Habrán dos personas de tipo Estudiante y una persona de tipo Profesor.
+Para ello crea una clase Persona que tenga una propiedad Nombre de tipo string, un constructor que reciba el nombre como parámetro y sobrescriba el método ToString().
+
+Después cree dos clases más que hereden de la clase Persona, se llamarán Estudiante y Profesor. La clase Estudiante tiene un método Estudiar que escribe por consola que el estudiante está estudiando. La clase Profesor tendrá un método Explicar que escribe en consola que el profesor está explicando. Recuerde crear además dos constructores en las clases hijas que llamen al constructor padre de la clase Persona.
+
+Finalice el programa leyendo las personas (el profesor y los alumnos) y ejecute los métodos de Explicar y Estudiar.
+
+Entrada
+Juan
+Sara
+Carlos
+Salida
+Explicar
+Estudiar
+Estudiar*/
 
 namespace Ejer7
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Persona[] personas = new Persona[3];
+            //{ new Estudiante("Juan"), new Estudiante("Sara"), new Profesor("Carlos") };
+            Profesor per1 = new Profesor("Juan");
+            personas[0] = per1;
+            Estudiante per2 = new Estudiante("Sara");
+            personas[1] = per2;
+            Estudiante per3 = new Estudiante("Carlos");
+            personas[2] = per3;
+
+            Console.WriteLine(per1.ToString());
+            per1.Explicar();
+            Console.WriteLine(per2.ToString());
+            per2.Estudiar();
+            Console.WriteLine(per3.ToString());
+            per3.Estudiar();
+
+            Console.ReadKey();
         }
     }
 }
